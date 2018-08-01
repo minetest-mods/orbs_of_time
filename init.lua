5123d6dd5cd2e0a5f3ff9c81deb88a555a612d71
texmex <josefsahlberg+minetest@gmail.com>
2018-08-01 15:25:52 +0200

    Spaces to tabs

--- init.lua
+++ init.lua
@@ -102,48 +102,48 @@ if minetest.get_modpath("mcl_core") ~= nil then
 	minetest.register_craft({
 		output = "orbs_of_time:orb_night",
 		recipe = {
-		 {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
-		 {"mcl_core:diamond", "dye:black","mcl_core:diamond"},
-		 {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
+			{"mcl_core:diamond", "dye:black","mcl_core:diamond"},
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
 		},
 	})
 
 else
 
 	minetest.register_craft( {
-	  output = "orbs_of_time:orb_day",
-	  recipe = {
-	    {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
-	    {"mcl_core:diamond", "default:mese_crystal","mcl_core:diamond"},
-	    {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
-	   },
+		output = "orbs_of_time:orb_day",
+		recipe = {
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
+			{"mcl_core:diamond", "default:mese_crystal","mcl_core:diamond"},
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
+		},
 	})
 
 	minetest.register_craft({
-	  output = "orbs_of_time:orb_night",
-	  recipe = {
-	   {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
-	   {"mcl_core:diamond", "default:obsidian_shard","mcl_core:diamond"},
-	   {"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
-	  },
+		output = "orbs_of_time:orb_night",
+		recipe = {
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"},
+			{"mcl_core:diamond", "default:obsidian_shard","mcl_core:diamond"},
+			{"mcl_core:diamond", "mcl_core:diamond","mcl_core:diamond"}
+		},
 	})
 
 end
 
 	minetest.register_craft({
-	  output = "orbs_of_time:orb_dawn 2",
-	  recipe = {
-	   {"orbs_of_time:orb_day"},
-	   {"orbs_of_time:orb_night"},
-	  },
+		output = "orbs_of_time:orb_dawn 2",
+		recipe = {
+			{"orbs_of_time:orb_day"},
+			{"orbs_of_time:orb_night"},
+		},
 	})
 
 	minetest.register_craft({
-	  output = "orbs_of_time:orb_dusk 2",
-	  recipe = {
-	   {"orbs_of_time:orb_night"},
-	   {"orbs_of_time:orb_day"},
-	  },
+		output = "orbs_of_time:orb_dusk 2",
+		recipe = {
+			{"orbs_of_time:orb_night"},
+			{"orbs_of_time:orb_day"},
+		},
 	})
 
 -----------
