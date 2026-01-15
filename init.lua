minetest-mods/orbs_of_time

commit 770ce36af6012739fa616ca0b2315a4c25a88b68

Support creative mods
This bumps the minimal engine version to 5.3, which is
almost 5 years old (at the time of commit).

init.lua
@@ -1,7 +1,19 @@
+assert(core.is_creative_enabled, "API requirement is not met. Please update Luanti/Minetest.")
+
 local S = minetest.get_translator(minetest.get_current_modname())
 
 minetest.register_alias("castle:orb_day", "orbs_of_time:orb_day")
 minetest.register_alias("castle:orb_night", "orbs_of_time:orb_night")
+
+local function generic_on_use(itemstack, user, sound2)
+	core.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
+	core.set_timeofday(0.5)
+	core.sound_play(sound2, {pos=user:get_pos(), loop=false})
+	if not core.is_creative_enabled(user:get_player_name()) then
+		itemstack:add_wear(65535/8)
+	end
+	return itemstack
+end
 
 minetest.register_tool("orbs_of_time:orb_day", {
 	description = S("Orb of Midday"),
@@ -17,13 +29,7 @@ minetest.register_tool("orbs_of_time:orb_day", {
 	stack_max=1,
 	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
-		minetest.set_timeofday(0.5)
-		minetest.sound_play("orbs_birds", {pos=user:get_pos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
-		end
-		return itemstack
+		return generic_on_use(itemstack, user, "orbs_birds")
 	end,
 })
 
@@ -40,13 +46,7 @@ minetest.register_tool("orbs_of_time:orb_night",{
 	stack_max=1,
 	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
-		minetest.set_timeofday(0)
-		minetest.sound_play("orbs_owl", {pos=user:get_pos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
-		end
-		return itemstack
+		return generic_on_use(itemstack, user, "orbs_owl")
 	end,
 })
 
@@ -63,13 +63,7 @@ minetest.register_tool("orbs_of_time:orb_dawn", {
 	wield_image = "orbs_orb_day_weild.png^[lowpart:75:orbs_orb_night_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
-		minetest.set_timeofday(0.2)
-		minetest.sound_play("orbs_birds", {pos=user:get_pos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
-		end
-		return itemstack
+		return generic_on_use(itemstack, user, "orbs_birds")
 	end,
 })
 
@@ -84,13 +78,7 @@ minetest.register_tool("orbs_of_time:orb_dusk",{
 	wield_image = "orbs_orb_night_weild.png^[lowpart:75:orbs_orb_day_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		minetest.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
-		minetest.set_timeofday(0.8)
-		minetest.sound_play("orbs_owl", {pos=user:get_pos(), loop=false})
-		if not minetest.settings:get_bool("creative_mode") then
-			itemstack:add_wear(65535/8)
-		end
-		return itemstack
+		return generic_on_use(itemstack, user, "orbs_owl")
 	end,
 })
 

mod.conf
@@ -1,3 +1,5 @@
 name = orbs_of_time
 optional_depends = default, mcl_core, loot, doc
 description = A mod containing magical orbs a player can use a limited number of times to change the time of day
+# For core.is_creative_enabled
+min_minetest_version = 5.3

commit 7ac02f0c542bd70af93d33e5b83da44142bd30ea

Fix all orbs setting same time (#9)
This fixes a regression caused by 770ce36.

init.lua
@@ -5,9 +5,9 @@ local S = minetest.get_translator(minetest.get_current_modname())
 minetest.register_alias("castle:orb_day", "orbs_of_time:orb_day")
 minetest.register_alias("castle:orb_night", "orbs_of_time:orb_night")
 
-local function generic_on_use(itemstack, user, sound2)
+local function generic_on_use(itemstack, user, time_of_day, sound2)
 	core.sound_play("orbs_ding", {pos=user:get_pos(), loop=false})
-	core.set_timeofday(0.5)
+	core.set_timeofday(time_of_day)
 	core.sound_play(sound2, {pos=user:get_pos(), loop=false})
 	if not core.is_creative_enabled(user:get_player_name()) then
 		itemstack:add_wear(65535/8)
@@ -29,7 +29,7 @@ minetest.register_tool("orbs_of_time:orb_day", {
 	stack_max=1,
 	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		return generic_on_use(itemstack, user, "orbs_birds")
+		return generic_on_use(itemstack, user, 0.5, "orbs_birds")
 	end,
 })
 
@@ -46,7 +46,7 @@ minetest.register_tool("orbs_of_time:orb_night",{
 	stack_max=1,
 	groups = { tool=1 },
 	on_use = function(itemstack, user)
-		return generic_on_use(itemstack, user, "orbs_owl")
+		return generic_on_use(itemstack, user, 0, "orbs_owl")
 	end,
 })
 
@@ -63,7 +63,7 @@ minetest.register_tool("orbs_of_time:orb_dawn", {
 	wield_image = "orbs_orb_day_weild.png^[lowpart:75:orbs_orb_night_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		return generic_on_use(itemstack, user, "orbs_birds")
+		return generic_on_use(itemstack, user, 0.2, "orbs_birds")
 	end,
 })
 
@@ -78,7 +78,7 @@ minetest.register_tool("orbs_of_time:orb_dusk",{
 	wield_image = "orbs_orb_night_weild.png^[lowpart:75:orbs_orb_day_weild.png",
 	stack_max=1,
 	on_use = function(itemstack, user)
-		return generic_on_use(itemstack, user, "orbs_owl")
+		return generic_on_use(itemstack, user, 0.8, "orbs_owl")
 	end,
 })
 